f2f55dbfea8d45c81257201ac83db158ed666a19
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-20 17:19:22 +1000

    feat: Add PlanetModel

---
+++ Galcon/Assets/Scripts/Planet/PlanetModel.cs
@@ -0,0 +1,13 @@
+﻿public class PlanetModel
+{
+    public float Radius { get; }
+    public int ShipsCount { get; set; }
+    public Player Owner { get; }
+
+    public PlanetModel(float radius, int shipsCount, Player owner = null)
+    {
+        ShipsCount = shipsCount;
+        Radius = radius;
+        Owner = owner;
+    }
+}
