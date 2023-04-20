192bcad3da6f895dcfbe85aa2f4a17c49e1d1e4d
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-20 18:28:30 +1000

    feat: Add PlanetFactory

---
+++ Galcon/Assets/Scripts/Planet/PlanetFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class PlanetFactory
+{
+    [Serializable]
+    public class Settings
+    {
+        public Sprite[] PlanetSprites;
+        public float minRadius;
+        public float maxRadius;
+        public int minAmount;
+        public int maxAmount;
+    }
+
+    private Settings _settings;
+    private PrefabProvider _prefabProvider;
+
+    public PlanetFactory(Settings settings, PrefabProvider prefabProvider)
+    {
+        _settings = settings;
+        _prefabProvider = prefabProvider;
+    }
+
+    public Planet Create(int shipsAmount, Player player)
+    {
+        var prefab = _prefabProvider.Load<Planet>(PrefabPath.Planet);
+        var planet = UnityEngine.Object.Instantiate(prefab);
+
+        var model = CreateModel(shipsAmount, player);
+
+        planet.Construct(model);
+        return planet;
+    }
+
+    private PlanetModel CreateModel(int shipsAmount, Player player)
+    {
+        var radius = UnityEngine.Random.Range(_settings.minRadius, _settings.maxRadius);
+        var model = new PlanetModel(radius, shipsAmount, player);
+        return model;
+    }
+}
