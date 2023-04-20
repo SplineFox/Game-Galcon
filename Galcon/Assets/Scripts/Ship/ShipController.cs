3548ad4eb9452ceecdde120fc67c6a0864ff4253
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-20 20:31:18 +1000

    feat: Add ShipController

---
+++ Galcon/Assets/Scripts/Ship/ShipController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private ShipView _view;
+
+    private Ship _model;
+
+    public void OnCreate(Ship model)
+    {
+        _model = model;
+        _model.DeleteRequested += OnDeleteRequested;
+    }
+
+    private void OnDestroy()
+    {
+        _model.DeleteRequested -= OnDeleteRequested;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_model != null)
+            UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        Vector2 prevPosition = _model.Position;
+
+        Vector2 direction = (_model.Target.Position - _model.Position).normalized;
+        Vector2 velocity = direction * _model.Speed * Time.deltaTime;
+        Vector2 position = _model.Position + velocity;
+
+        _rigidbody2D.MovePosition(position);
+        _model.SetPosition(_rigidbody2D.position);
+    }
+
+    private void OnDeleteRequested()
+    {
+        Destroy(gameObject);
+    }
+}
