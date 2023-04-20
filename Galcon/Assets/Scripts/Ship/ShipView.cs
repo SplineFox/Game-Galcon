b240b92e155595161d46a0a6455d87f8acba7d59
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-20 20:31:14 +1000

    feat: Add ShipView

---
+++ Galcon/Assets/Scripts/Ship/ShipView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ShipView : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    public void SetColor(Color color)
+    {
+        _spriteRenderer.color = color;
+    }
+}
