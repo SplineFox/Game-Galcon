eff74ebc0bfaf40ec53aa26502b2da3f9ce55586
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-21 09:37:19 +1000

    feat: Add empty GameLoopState

---
+++ Galcon/Assets/Scripts/Infrastructure/Game/States/GameLoopState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameLoopState : IGameState
+{
+    private readonly GameStateMachine _stateMachine;
+
+    public GameLoopState(GameStateMachine stateMachine)
+    {
+        _stateMachine = stateMachine;
+    }
+
+    public void Enter()
+    {
+    }
+
+    public void Exit()
+    {
+    }
+}
