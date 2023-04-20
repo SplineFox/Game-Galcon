2a8dc10ac646ee7cdaf05866d1b8cb6e88ef9760
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-20 19:59:29 +1000

    feat: Add LevelArea

---
+++ Galcon/Assets/Scripts/Level/ILevelArea.cs
@@ -0,0 +1,18 @@
+﻿public interface ILevelArea
+{
+    public float Bottom { get; }
+
+    public float Top { get; }
+
+    public float Left { get; }
+
+    public float Right { get; }
+
+    public float ExtentHeight { get; }
+
+    public float Height { get; }
+
+    public float ExtentWidth { get; }
+
+    public float Width { get; }
+}
---
+++ Galcon/Assets/Scripts/Level/LevelArea.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+public class LevelArea : ILevelArea
+{
+    [Serializable]
+    public class Settings
+    {
+        public float OffsetFromEdges;
+    }
+
+    private readonly Settings _settings;
+    private readonly Camera _camera;
+
+    public LevelArea(Settings settings, Camera camera)
+    {
+        _settings = settings;
+        _camera = camera;
+    }
+
+    public float Bottom
+    {
+        get { return -ExtentHeight; }
+    }
+
+    public float Top
+    {
+        get { return ExtentHeight; }
+    }
+
+    public float Left
+    {
+        get { return -ExtentWidth; }
+    }
+
+    public float Right
+    {
+        get { return ExtentWidth; }
+    }
+
+    public float ExtentHeight
+    {
+        get { return _camera.orthographicSize - _settings.OffsetFromEdges; }
+    }
+
+    public float Height
+    {
+        get { return ExtentHeight * 2.0f; }
+    }
+
+    public float ExtentWidth
+    {
+        get { return _camera.aspect * _camera.orthographicSize - _settings.OffsetFromEdges; }
+    }
+
+    public float Width
+    {
+        get { return ExtentWidth * 2.0f; }
+    }
+}
