dfb41412cfa73e3f67c1998b8b31f8aa28274bdf
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-21 09:03:06 +1000

    feat: Add GameSettings

---
+++ Galcon/Assets/Scripts/Infrastructure/Game/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "Settings/GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    [Header("Game-related settings")]
+    public LevelArea.Settings LevelAreaSettings;
+    
+    [Space]
+    [Header("Planet-related settings")]
+    public PlanetFactory.Settings PlanetFactorySettings;
+    public PlanetSpawner.Settings PlanetSpawnerSettings;
+    
+    [Space]
+    [Header("Ship-related settings")]
+    public ShipFactory.Settings ShipFactorySettings;
+    public ShipSpawner.Settings ShipSpawnerSettings;
+}
