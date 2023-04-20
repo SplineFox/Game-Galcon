fe7105bc172367e9727345c3408c7d976f32f5e3
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-20 20:30:49 +1000

    feat: Add Ship

---
+++ Galcon/Assets/Scripts/Ship/Ship.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class Ship
+{
+    public Vector2 Position { get; private set; }
+    public float Speed { get; }
+    public Planet Target { get; }
+    public Player Owner { get; }
+
+
+    public event Action DeleteRequested = delegate { };
+
+
+    public Ship(float speed, Planet target, Player owner)
+    {
+        Speed = speed;
+        Target = target;
+        Owner = owner;
+    }
+
+    public void SetPosition(Vector2 position)
+    {
+        Position = position;
+    }
+
+    public void Delete()
+    {
+        DeleteRequested.Invoke();
+    }
+
+}
