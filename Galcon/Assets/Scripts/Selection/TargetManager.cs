00cfe03e12db9f1d0110c64730a61e4f43a27ea4
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-21 19:05:50 +1000

    mod: Delete TargetState

--- Galcon/Assets/Scripts/Selection/TargetManager.cs
+++ Galcon/Assets/Scripts/Selection/TargetManager.cs
@@ -2,69 +2,73 @@
 
 public class TargetManager : ITargetManager
 {
-    private readonly Player _player;
+    private readonly IPlayersRegistry _playersRegistry;
     private readonly IShipSpawner _shipSpawner;
 
-    private TargetState _state;
     private List<Planet> _selectedPlanets;
 
-    public TargetManager(Player player, IShipSpawner shipSpawner)
+    public TargetManager(IPlayersRegistry playersRegistry, IShipSpawner shipSpawner)
     {
-        _player = player;
+        _playersRegistry = playersRegistry;
         _shipSpawner = shipSpawner;
 
-        _state = TargetState.Idling;
         _selectedPlanets = new List<Planet>();
     }
 
     public void Select(Planet planet)
     {
-        switch (_state)
+        if(IsTargeting())
         {
-            case TargetState.Idling:
-                SelectPlanet(planet);
-                _state = TargetState.Targeting;
-                break;
-            case TargetState.Targeting:
-                SendShips(planet);
-                DeselectAll();
-                _state = TargetState.Idling;
-                break;
+            SendShips(planet);
+            DeselectAll();
+        }
+        else
+        {
+            SelectPlanet(planet);
         }
     }
 
     public void SelectMultiple(List<Planet> planets)
     {
-        foreach (var planet in _selectedPlanets)
-            SelectPlanet(planet);
-
-        _state = TargetState.Targeting;
+        for (int index = 0; index < planets.Count; index++)
+            SelectPlanet(planets[index]);
     }
 
     public void DeselectAll()
     {
-        foreach (var planet in _selectedPlanets)
-            DeselectPlanet(planet);
+        for (int index = _selectedPlanets.Count - 1; index >= 0; index--)
+            DeselectPlanet(_selectedPlanets[index]);
+    }
 
-        _selectedPlanets.Clear();
-        _state = TargetState.Idling;
+    private bool IsTargeting()
+    {
+        return _selectedPlanets.Count > 0;
     }
 
     private void SelectPlanet(Planet planet)
     {
-        planet.SetSelection(true);
+        if(planet.Owner == _playersRegistry.MainPlayer)
+        {
+            planet.SetSelection(true);
+            _selectedPlanets.Add(planet);
+        }
     }
 
     private void DeselectPlanet(Planet planet)
     {
-        planet.SetSelection(false);
+        if (planet.Owner == _playersRegistry.MainPlayer)
+        {
+            planet.SetSelection(false);
+            _selectedPlanets.Remove(planet);
+        }
     }
 
     private void SendShips(Planet targetPlanet)
     {
         foreach (var planet in _selectedPlanets)
         {
-            _shipSpawner.GenerateSquadron(planet, targetPlanet);
+            if(planet != targetPlanet)
+                _shipSpawner.GenerateSquadron(planet, targetPlanet);
         }
     }
 }
