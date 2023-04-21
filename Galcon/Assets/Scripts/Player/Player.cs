29c1be5840e9d3f8ba65c086fc6c923403401ebf
SplineFox <89731027+SplineFox@users.noreply.github.com>
2023-04-21 14:43:25 +1000

    feat: Add list of planets to Player

--- Galcon/Assets/Scripts/Player/Player.cs
+++ Galcon/Assets/Scripts/Player/Player.cs
@@ -1,6 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Player
 {
-    public Color Color { get; }
+    private Color _color;
+    private List<Planet> _planets;
+
+    public Color Color => _color;
+    public int NumberOfPlanets => _planets.Count;
+
+    public Player(Color color)
+    {
+        _planets = new List<Planet>();
+    }
+
+    public void AddPlanet(Planet planet)
+    {
+        if (!HasPlanet(planet))
+            _planets.Add(planet);
+    }
+
+    public void RemovePlanet(Planet planet)
+    {
+        if(HasPlanet(planet))
+            _planets.Remove(planet);
+    }
+
+    public bool HasPlanet(Planet planet)
+    {
+        return _planets.Contains(planet);
+    }
 }
